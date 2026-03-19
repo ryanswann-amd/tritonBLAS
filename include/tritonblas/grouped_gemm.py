6460e0d76f5b798fc3cf43b5bc546f00b7d8bcfc
Ryan Swann <ryanswann@amd.com>
2026-03-19 13:35:00 +0000

    Revert "optimize-loop iter 10: num_warps=4 + waves_per_eu=2 (constrain register pressure)"
    This reverts commit 984573ba5db4e79ddc9afe74baba6d02b0b93b70.

--- include/tritonblas/grouped_gemm.py
+++ include/tritonblas/grouped_gemm.py
@@ -116,8 +116,8 @@ def _heterogeneous_dispatch(group_a, group_b, group_c, group_shapes, group_size,
         NUM_SMS=MAX_SMS, NUM_XCDS=_NUM_XCDS, CHUNK_SIZE=chunk_size,
         MATMUL_DTYPE=triton_dtype,
         EVEN_K=even_k,
-        num_stages=2, num_warps=4,
-        waves_per_eu=2, matrix_instr_nonkdim=16, kpack=1,
+        num_stages=2, num_warps=8,
+        waves_per_eu=0, matrix_instr_nonkdim=16, kpack=1,
     )
 
 
